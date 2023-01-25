9714ebc5a4c3c32c4b9fa7d998327959c6d40092
Krzysiek Gesicki <krzysiek.gesicki@gmail.com>
2023-01-25 22:39:09 +0100

    added some excercise code

--- SecondConsoleApp/Program.cs
+++ SecondConsoleApp/Program.cs
@@ -10,7 +10,44 @@ namespace SecondConsoleApp
     {
         static void Main(string[] args)
         {
+            Person Jason = new Person();
+            Jason.Name = "Jason";
+            Jason.Age = 15;
+            Console.WriteLine($"Persons name is: {Jason.Name}");
+            Console.WriteLine($"Persons age is: {Jason.Age}");
 
+            Console.ReadLine();
         }
+    }
+
+    class Person
+    {
+        private string name;
+        private int age;
+
+        //constructor example:
+        public Person()
+        {
+            age = 0;
+        }
+
+
+
+        //property example:
+        
+        public string Name { get; set; }
+
+        public int Age
+        {
+            get { return age; }
+            set { 
+                if (value > 0)
+                age = value; 
+            }
+        }
+
+
+
+
     }
 }
