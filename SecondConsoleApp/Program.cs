fc1f62017c3aa2ec0e195b66b46dd14f2227a2f2
Krzysiek Gesicki <krzysiek.gesicki@gmail.com>
2023-01-27 14:30:24 +0100

    other examples

--- SecondConsoleApp/Program.cs
+++ SecondConsoleApp/Program.cs
@@ -17,6 +17,66 @@ namespace SecondConsoleApp
             Console.WriteLine($"Persons age is: {Jason.Age}");
 
             Console.ReadLine();
+
+
+            //array example
+
+            string[] names = new string[3] { "Morpheus", "Thomas", "Oracle" };
+
+            string[] nicknames = new string[] { "Homelander", "A-Train", "Starlight" };
+
+            string[] surnames = {"House", "Wilson", "Cuddy"};
+
+            foreach (var n1 in names)
+            {
+                Console.WriteLine(n1);
+            }
+            foreach (string n2 in nicknames)
+            {
+                Console.WriteLine(n2);
+            }
+            foreach (var n3 in surnames)
+            {
+                Console.WriteLine(n3);
+            }
+            Console.ReadLine();
+
+
+
+
+
+            //array example - multidimensional
+
+            int[,] x = new int[3, 4]; 
+
+            int[,] y = { {3,5}, {6,8}, {7,9}, {8,9} };
+
+            for (int i=0; i<4; i++) // rows
+            {
+                for (int j=0; j<2; j++) // columns
+                {
+                    Console.Write(y[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+
+            int[] z = { 2, 44, 1, 9 };
+
+            Console.WriteLine("rank= " + z.Rank);
+            Console.WriteLine("length= " + z.Length);
+
+            Console.WriteLine("max= " + z.Max());
+            Console.WriteLine("min= " + z.Min());
+            Console.WriteLine("sum= " + z.Sum());
+            Console.ReadLine();
+
+         
+
+
+
+
+
         }
     }
 
